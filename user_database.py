f55a957ca61c84181e62285fc4d9ecada5e59704
ckruger097 <ckruger4@gmu.edu>
2021-05-13 14:55:23 -0400

    updated requirements.txt and removed old comments

--- user_database.py
+++ user_database.py
@@ -22,6 +22,7 @@ def get_all_users():
     all_user_keys = collection.find({})
     return list(all_user_keys)
 
+
 def add_user(user):
     return collection.insert_one(user)
 
@@ -59,63 +60,3 @@ def get_user_currency(user):
 
 def get_user_starting_balance(user):
     return user.get("starting_balance")
-
-# import currency
-
-# db = pdb.load('bogged_user_database.db', True)
-# accepted_currencies = ['usd', 'ars', 'aud', 'cad', 'chf', 'clp', 'cny', 'czk', 'dkk', 'eur', 'gbp', 'hkd', 'inr',
-#                        'jpy', 'krw', 'kwd', 'mmk', 'mxn', 'myr', 'ngn', 'nok', 'nzd', 'pkr', 'pln', 'rub', 'sar',
-#                        'sek', 'sgd', 'thb', 'try', 'twd', 'uah', 'vef', 'vnd', 'zar']
-
-
-# def add_user(name, email, password, confirmed_password, user_currency, balance):
-#     if db.exists(name):
-#         return "USER_EXISTS"
-#     if user_currency.lower() not in accepted_currencies:
-#         return "BAD_CURRENCY"
-#     if password != confirmed_password:
-#         return "BAD_PASSWORD"
-#     if "@" not in email:
-#         return "BAD_EMAIL"
-#     else:
-#         now = datetime.now()
-#         regtime = now.strftime("%m/%d/%Y, %H:%M:%S")
-#         _id = len(db.getall()) + 1
-#         wallet = {
-#             "bitcoin": 0,
-#             "litecoin": 0,
-#             "ethereum": 0
-#         }
-#         user_info = {
-#             "name": name,
-#             "id": int(_id),
-#             "email": email,
-#             "password": password,
-#             "currency": user_currency.lower(),
-#             "wallet": wallet,
-#             "starting_balance": int(balance),
-#             "balance": int(balance),
-#             "date_joined": regtime
-#         }
-#         # print(now.strftime("%m/%d/%Y, %H:%M:%S"))
-#         db.set(name, user_info)
-#         return "OK"
-
-# def get_id(name):
-#         return db.get(name).get("id")
-
-# def get_username(name):
-#         return db.get(name).get("name")
-
-# def get_user(name, given_password):
-#     if not db.exists(name):
-#         print("This user does not exist.")
-#         return False
-#     else:
-#         if db.get(name).get("password") != given_password:
-#             print("Incorrect password")
-#             return False
-#         else:
-#             return db.get(name)
-
-
